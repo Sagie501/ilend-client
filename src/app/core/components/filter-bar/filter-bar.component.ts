5ce7f804ae951d24b7574fba66f64a04a12023e5
tomshudev <tomshudev@gmail.com>
2020-04-09 19:08:38 +0300

    Update the filter bar design and functionality

--- src/app/core/components/filter-bar/filter-bar.component.ts
+++ src/app/core/components/filter-bar/filter-bar.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { PriceFilter } from '../../reducers/filter.reducer';
 
 @Component({
   selector: 'ile-filter-bar',
   templateUrl: './filter-bar.component.html',
-  styleUrls: ['./filter-bar.component.less']
+  styleUrls: ['./filter-bar.component.less'],
 })
 export class FilterBarComponent implements OnInit {
+  @Output() searchFilter: EventEmitter<string> = new EventEmitter<string>();
+  @Output() priceFilter: EventEmitter<PriceFilter> = new EventEmitter<
+    PriceFilter
+  >();
+  @Output() categoryFilter: EventEmitter<string> = new EventEmitter<string>();
+  @Output() cityFilter: EventEmitter<string> = new EventEmitter<string>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  search(value: string) {
+    this.searchFilter.emit(value);
   }
-
 }
