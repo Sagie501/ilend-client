c38a982b903484b2cc44bc10ba5924a4f2efc95b
tomshudev <tomshudev@gmail.com>
2020-04-10 19:05:07 +0300

    Update the shell

---
+++ src/app/features/filter-bar/models/options.model.ts
@@ -0,0 +1,6 @@
+export type Option = {
+  title: string;
+  id: string;
+  isChecked: boolean;
+  amount?: number;
+};
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './components/search/search.component';
+import { CheckboxComponent } from './components/checkbox/checkbox.component';
+import { AmountComponent } from './components/amount/amount.component';
 
 @NgModule({
-  declarations: [SearchComponent],
+  declarations: [SearchComponent, CheckboxComponent, AmountComponent],
   imports: [CommonModule],
-  exports: [SearchComponent],
+  exports: [SearchComponent, CheckboxComponent, AmountComponent],
 })
 export class SharedModule {}
