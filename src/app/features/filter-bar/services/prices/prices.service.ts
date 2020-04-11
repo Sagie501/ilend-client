a28bc9df0eb0e5a22d67a38deaa4b46a635c23cf
tomshudev <tomshudev@gmail.com>
2020-04-11 18:07:03 +0300

    Create the prices service that sets the ranges

---
+++ src/app/features/filter-bar/services/prices/prices.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Product } from 'src/app/core/models/product.model';
+
+const GROUP_NUMBER = 50;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PricesService {
+
+  prices = []
+
+  constructor() {
+    for (let index = 0; index < 200; index++) {
+      this.prices.push(Math.random() * 1000);
+    }
+  }
+
+  getPricesRanges(products: Product[]): number[] {
+    // Get all the prices
+    // let prices: number[] = products.map((product) => product.requestedPrice);
+
+    let jumpValue = this.getJumpValue(products);
+
+    let values = new Array(GROUP_NUMBER).fill(0);
+
+    this.prices.forEach(currPrice => {
+      let index = Math.floor(currPrice / jumpValue) === 0 ? Math.floor(currPrice / jumpValue) : Math.floor(currPrice / jumpValue) - 1;
+
+      values[index] += 1;
+    });
+
+    return values;
+  }
+
+  getJumpValue(products: Product[]): number {
+    this.prices.sort((a, b) => a - b);
+
+    return Math.floor(this.prices[this.prices.length - 1] / GROUP_NUMBER);
+  }
+}
