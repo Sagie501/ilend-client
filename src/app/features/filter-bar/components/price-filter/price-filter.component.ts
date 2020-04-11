2757082d5d8afb3228ad13a7abf1778e41bc3494
tomshudev <tomshudev@gmail.com>
2020-04-11 18:10:28 +0300

    Create the price filter component

---
+++ src/app/features/filter-bar/components/price-filter/price-filter.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, Input, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { PriceFilterOverlayService, PriceFilterOverlayConfig } from '../../services/overlay/price/price-filter-overlay.service';
+import { Product } from 'src/app/core/models/product.model';
+import { PriceFilter } from '../../models/price-filter.model';
+import { PricesService } from '../../services/prices/prices.service';
+
+@Component({
+  selector: 'ile-price-filter',
+  templateUrl: './price-filter.component.html',
+  styleUrls: ['./price-filter.component.less']
+})
+export class PriceFilterComponent implements OnInit {
+
+  @Input() products: Product[];
+  @Input() currentMinimum: number;
+  @Input() currentMaximum: number;
+
+  @Output() changed: EventEmitter<PriceFilter> = new EventEmitter();
+
+  @ViewChild('price') price: ElementRef;
+
+  constructor(private priceFilterOverlayService: PriceFilterOverlayService, private pricesService: PricesService) { }
+
+  ngOnInit(): void {
+  }
+
+  openOverlay() {
+    this.priceFilterOverlayService.open(
+      {
+        data: this.pricesService.getPricesRanges(this.products),
+        currentMinimum: /*this.currentMinimum*/0,
+        currentMaximum: /*this.currentMaximum*/this.pricesService.getPricesRanges(this.products).length,
+        jumpValue: this.pricesService.getJumpValue(this.products),
+        callback: (change: PriceFilter) => this.changed.emit(change),
+      } as PriceFilterOverlayConfig,
+      this.price
+    );
+  }
+
+}
