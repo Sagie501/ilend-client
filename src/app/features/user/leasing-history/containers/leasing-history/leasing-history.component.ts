570fcfdb3f80f9b3985cb20d7ad3e62443281389
tomshudev <tomshudev@gmail.com>
2020-04-12 17:46:10 +0300

    Create the module and component of leasing-history

---
+++ src/app/features/user/leasing-history/containers/leasing-history/leasing-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ile-leasing-history',
+  templateUrl: './leasing-history.component.html',
+  styleUrls: ['./leasing-history.component.less']
+})
+export class LeasingHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
