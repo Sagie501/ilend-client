a90c0bc1e57d492c6cdb6948fecb40d3638daae8
tomshudev <tomshudev@gmail.com>
2020-04-13 16:08:49 +0300

    Display the leasings table

--- src/app/features/user/leasing-history/leasing-history.module.ts
+++ src/app/features/user/leasing-history/leasing-history.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LeasingHistoryComponent } from './containers/leasing-history/leasing-history.component';
 import { LeaseProductDisplayComponent } from './components/lease-product-display/lease-product-display.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { LeasingsTableComponent } from './components/leasings-table/leasings-table.component';
 
 @NgModule({
-  declarations: [LeasingHistoryComponent, LeaseProductDisplayComponent],
+  declarations: [LeasingHistoryComponent, LeaseProductDisplayComponent, LeasingsTableComponent],
   imports: [CommonModule, SharedModule],
 })
 export class LeasingHistoryModule {}
