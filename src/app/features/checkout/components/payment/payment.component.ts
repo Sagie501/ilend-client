291618196f6f21de930ee36c33cefd13a9140ac7
tomshudev <tomshudev@gmail.com>
2020-04-15 16:36:12 +0300

    Create the payment view component

---
+++ src/app/features/checkout/components/payment/payment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ile-payment',
+  templateUrl: './payment.component.html',
+  styleUrls: ['./payment.component.less']
+})
+export class PaymentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
