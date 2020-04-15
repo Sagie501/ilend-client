92ab712bda43f4440fb5c473bc28134f6628c721
tomshudev <tomshudev@gmail.com>
2020-04-15 16:35:53 +0300

    Create the credit card simulator component

---
+++ src/app/features/checkout/components/credit-card-simulator/credit-card-simulator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ile-credit-card-simulator',
+  templateUrl: './credit-card-simulator.component.html',
+  styleUrls: ['./credit-card-simulator.component.less']
+})
+export class CreditCardSimulatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
