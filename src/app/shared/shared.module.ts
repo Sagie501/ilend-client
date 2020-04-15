fe32f1ea3c9f4be3440755f9d4420d2cfba1736e
tomshudev <tomshudev@gmail.com>
2020-04-15 16:34:44 +0300

    Move the header to the shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,21 @@ import { CommonModule } from '@angular/common';
 import { SearchComponent } from './components/search/search.component';
 import { CheckboxComponent } from './components/checkbox/checkbox.component';
 import { AmountComponent } from './components/amount/amount.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [SearchComponent, CheckboxComponent, AmountComponent],
+  declarations: [
+    SearchComponent,
+    CheckboxComponent,
+    AmountComponent,
+    HeaderComponent,
+  ],
   imports: [CommonModule],
-  exports: [SearchComponent, CheckboxComponent, AmountComponent],
+  exports: [
+    SearchComponent,
+    CheckboxComponent,
+    AmountComponent,
+    HeaderComponent,
+  ],
 })
 export class SharedModule {}
