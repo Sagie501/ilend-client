5bbd9225a803a8a89c6eaf885d7b0be711e95400
nivhindi <nivhindi1@gmail.com>
2020-04-18 15:44:23 +0300

    Created the account page

---
+++ src/app/core/components/account-page/account-page/account-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ile-account-page',
+  templateUrl: './account-page.component.html',
+  styleUrls: ['./account-page.component.less']
+})
+export class AccountPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -11,6 +11,10 @@ import { UserService } from './services/user/user.service';
 import { FilterBarModule } from '../features/filter-bar/filter-bar.module';
 import { HomeComponent } from './containers/home/home.component';
 import { DataOutletComponent } from './containers/data-outlet/data-outlet.component';
+import { AccountPageComponent } from './components/account-page/account-page/account-page.component';
+import {MatDividerModule} from '@angular/material/divider';
+
+
 
 @NgModule({
   declarations: [
@@ -20,8 +24,9 @@ import { DataOutletComponent } from './containers/data-outlet/data-outlet.compon
     MainComponent,
     HomeComponent,
     DataOutletComponent,
+    AccountPageComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, FilterBarModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, FilterBarModule, MatDividerModule],
   providers: [UserService],
 })
 export class CoreModule {}
