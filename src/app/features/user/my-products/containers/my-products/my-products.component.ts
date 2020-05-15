356928fa6f527788b1fb515300dc3073589616b9
Sagie <Sagie012@gmail.com>
2020-05-16 02:21:09 +0300

    Create basic new product dialog

--- src/app/features/user/my-products/containers/my-products/my-products.component.ts
+++ src/app/features/user/my-products/containers/my-products/my-products.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { getGreetingSentence } from 'src/app/shared/helpers/greeting-sentence.helper';
+import { User } from '../../../../../core/models/user.model';
+import { getLoggedInUser, getUserProducts, UserState } from '../../../reducer/user.reducer';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { Product } from '../../../../../core/models/product.model';
+import { MatDialog } from '@angular/material/dialog';
+import { NewProductDialogComponent } from '../../components/new-product-dialog/new-product-dialog.component';
 
 @Component({
   selector: 'ile-my-products',
   templateUrl: './my-products.component.html',
   styleUrls: ['./my-products.component.less']
 })
-export class MyProductsComponent implements OnInit {
+export class MyProductsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  loggedInUser: User;
+  userProducts: Array<Product>;
+  getGreetingSentence = getGreetingSentence;
+  subscriptions: Array<Subscription>;
+
+  constructor(private userStore: Store<UserState>, private dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.subscriptions = [
+      this.userStore.select(getLoggedInUser).subscribe((loggedInUser) => {
+        this.loggedInUser = loggedInUser;
+      }),
+      this.userStore.select(getUserProducts).subscribe((userProducts) => {
+        this.userProducts = userProducts;
+      })
+    ];
   }
 
+  openNewProductDialog() {
+    this.dialog.open(NewProductDialogComponent, {
+      autoFocus: false,
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+  }
 }
--- src/app/features/user/my-products/my-products.module.ts
+++ src/app/features/user/my-products/my-products.module.ts
@@ -1,13 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyProductsComponent } from './containers/my-products/my-products.component';
-
+import { SharedModule } from '../../../shared/shared.module';
+import { NewProductDialogComponent } from './components/new-product-dialog/new-product-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MaterialFileInputModule } from 'ngx-material-file-input';
 
 
 @NgModule({
-  declarations: [MyProductsComponent],
+  declarations: [MyProductsComponent, NewProductDialogComponent],
+  entryComponents: [NewProductDialogComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MaterialFileInputModule
   ]
 })
 export class MyProductsModule { }
