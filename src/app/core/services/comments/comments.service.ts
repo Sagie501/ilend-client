e6696852d12e1971c0e1011305159b698eac6759
Sagie <Sagie012@gmail.com>
2020-05-21 23:27:44 +0300

    Add comments to product-page

--- src/app/core/services/comments/comments.service.ts
+++ src/app/core/services/comments/comments.service.ts
@@ -1,21 +1,40 @@
 import { Injectable } from '@angular/core';
 import { Comment } from '../../models/comment.model';
+import { Observable } from 'rxjs';
+import { Product } from '../../models/product.model';
+import { addProductMutation } from '../../graphql/product.graphql';
+import { map } from 'rxjs/operators';
+import { Apollo } from 'apollo-angular';
+import { addCommentMutation } from '../../graphql/comment.graphql';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentsService {
 
-  constructor() { }
+  constructor(private apollo: Apollo) { }
+
+  addComment(userId: string, productId: string, comment: string): Observable<Comment> {
+    return this.apollo.mutate<any>({
+      mutation: addCommentMutation,
+      variables: {
+        userId,
+        productId,
+        comment
+      }
+    }).pipe(
+      map(({ data, errors }) => {
+        return this.mapCommentForClient(data.addComment);
+      })
+    );
+  }
 
   mapCommentForClient(serverComment): Comment {
     let clientComment = {
       ...serverComment,
       date: new Date(serverComment.date),
-      userId: serverComment.user.id,
       productId: serverComment.product.id
     };
-    delete clientComment.user;
     delete clientComment.product;
     return clientComment;
   }
