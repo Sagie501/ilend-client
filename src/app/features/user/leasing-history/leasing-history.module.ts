263c7d11b304134897ef05d07368f6585d76349d
tomshudev <tomshudev@gmail.com>
2020-05-27 19:08:42 +0300

    Adapting the leasings-history to the new leasings table

--- src/app/features/user/leasing-history/leasing-history.module.ts
+++ src/app/features/user/leasing-history/leasing-history.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LeasingHistoryComponent } from './containers/leasing-history/leasing-history.component';
 import { LeaseProductDisplayComponent } from './components/lease-product-display/lease-product-display.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { LeasingsTableComponent } from './components/leasings-table/leasings-table.component';
+import { LeasingsTableComponent } from '../../../shared/components/leasings-table/leasings-table.component';
 
 @NgModule({
-  declarations: [LeasingHistoryComponent, LeaseProductDisplayComponent, LeasingsTableComponent],
+  declarations: [LeasingHistoryComponent, LeaseProductDisplayComponent],
   imports: [CommonModule, SharedModule],
 })
 export class LeasingHistoryModule {}
