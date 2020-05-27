2303bc942281ec0ca6b6cacda2f114c2872674b0
tomshudev <tomshudev@gmail.com>
2020-05-27 19:09:09 +0300

    Display all the ongoing leasings in a single page

--- src/app/features/ongoing-leasings/containers/ongoing-leasings/ongoing-leasings.component.ts
+++ src/app/features/ongoing-leasings/containers/ongoing-leasings/ongoing-leasings.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { LeasingService } from 'src/app/core/services/leasing/leasing.service';
+import { Store } from '@ngrx/store';
+import {
+  UserState,
+  getLoggedInUser,
+} from 'src/app/features/user/reducer/user.reducer';
+import { Leasing } from 'src/app/core/models/leasing.model';
+import { User } from 'src/app/core/models/user.model';
+import { Subscription, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { getGreetingSentence } from 'src/app/shared/helpers/greeting-sentence.helper';
 
 @Component({
   selector: 'ile-ongoing-leasings',
   templateUrl: './ongoing-leasings.component.html',
-  styleUrls: ['./ongoing-leasings.component.less']
+  styleUrls: ['./ongoing-leasings.component.less'],
 })
 export class OngoingLeasingsComponent implements OnInit {
+  leasings: Array<Leasing> = [];
+  loggedInUser: User;
+  subscriptions: Array<Subscription>;
+  getGreetingSentence = getGreetingSentence;
 
-  constructor() { }
+  constructor(
+    private leasingService: LeasingService,
+    private userStore: Store<UserState>
+  ) {}
 
   ngOnInit(): void {
+    this.subscriptions = [
+      this.userStore.select(getLoggedInUser).subscribe((loggedInUser) => {
+        this.loggedInUser = loggedInUser;
+      }),
+      this.userStore
+        .select(getLoggedInUser)
+        .pipe(
+          switchMap((loggedInUser) => {
+            if (loggedInUser) {
+              return this.leasingService.getAllOnGoingRequests(loggedInUser.id);
+            } else {
+              return of([] as Array<Leasing>);
+            }
+          })
+        )
+        .subscribe((leasings) => {
+          this.leasings = leasings;
+        }),
+    ];
   }
-
 }
--- src/app/features/ongoing-leasings/ongoing-leasings.module.ts
+++ src/app/features/ongoing-leasings/ongoing-leasings.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OngoingLeasingsComponent } from './containers/ongoing-leasings/ongoing-leasings.component';
-
-
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [OngoingLeasingsComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, SharedModule],
 })
-export class OngoingLeasingsModule { }
+export class OngoingLeasingsModule {}
