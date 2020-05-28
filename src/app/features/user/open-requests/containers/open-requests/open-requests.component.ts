b1175c330f6dbcd34a081929f1e476d9e86fe5b5
tomshudev <tomshudev@gmail.com>
2020-05-28 11:58:40 +0300

    Continue design open-requests

--- src/app/features/user/open-requests/containers/open-requests/open-requests.component.ts
+++ src/app/features/user/open-requests/containers/open-requests/open-requests.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { LeasingService } from 'src/app/core/services/leasing/leasing.service';
+import { Store } from '@ngrx/store';
+import {
+  UserState,
+  getLoggedInUser,
+} from 'src/app/features/user/reducer/user.reducer';
+import { Leasing } from 'src/app/core/models/leasing.model';
+import { User } from 'src/app/core/models/user.model';
+import { LeasingStatusFromServer } from '../../../../../shared/helpers/order-status.helper';
+import { Subscription, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { getGreetingSentence } from '../../../../../shared/helpers/greeting-sentence.helper';
 
 @Component({
   selector: 'ile-open-requests',
   templateUrl: './open-requests.component.html',
-  styleUrls: ['./open-requests.component.less']
+  styleUrls: ['./open-requests.component.less'],
 })
 export class OpenRequestsComponent implements OnInit {
+  leasings: Array<Leasing> = [];
+  loggedInUser: User;
+  subscriptions: Array<Subscription>;
+  getGreetingSentence = getGreetingSentence;
 
-  constructor() { }
+  constructor(
+    private leasingService: LeasingService,
+    private userStore: Store<UserState>
+  ) {}
 
   ngOnInit(): void {
+    this.subscriptions = [
+      this.userStore.select(getLoggedInUser).subscribe((loggedInUser) => {
+        this.loggedInUser = loggedInUser;
+      }),
+      this.userStore
+        .select(getLoggedInUser)
+        .pipe(
+          switchMap((loggedInUser) => {
+            if (loggedInUser) {
+              return this.leasingService.getAllOpenedRequests(loggedInUser.id);
+            } else {
+              return of([] as Array<Leasing>);
+            }
+          })
+        )
+        .subscribe((leasings) => {
+          this.leasings = leasings;
+        }),
+    ];
   }
 
+  changeLeasingRequestStatus(value: {
+    leasingId: string;
+    status: LeasingStatusFromServer;
+  }) {
+    this.leasingService
+      .setLeaseRequestStatus(value.leasingId, value.status)
+      .subscribe();
+  }
 }
--- src/app/features/user/open-requests/open-requests.module.ts
+++ src/app/features/user/open-requests/open-requests.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OpenRequestsComponent } from './containers/open-requests/open-requests.component';
-
-
+import { SharedModule } from '../../../shared/shared.module';
 
 @NgModule({
   declarations: [OpenRequestsComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, SharedModule],
 })
-export class OpenRequestsModule { }
+export class OpenRequestsModule {}
